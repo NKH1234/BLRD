0d5073c5bc2518c3201e25f1fa6690c929910943
NKH1234 <156617449+NKH1234@users.noreply.github.com>
2024-03-07 11:35:39 -0600

    Update pxel.js

--- pxel.js
+++ pxel.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const submitButton = document.querySelector('.guess-container button');
   const pixelatedImage = document.getElementById('pixelated-image');
   const timerDisplay = document.getElementById('timer');
-  const correctAnswer = "MAP";
+  const correctAnswer = "SEATTLE";
  
  
   let timeLeft = 90;
